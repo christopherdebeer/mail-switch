3c740f0d5ee6aa3648fa16d53bb23862058bb1ac
Christopher de Beer <christopherdebeer@gmail.com>
2013-09-07 16:38:39 +0000

    Clean up folder structure and default files

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ if (!process.env.MANDRILL_API_KEY) process.exit(1);
 
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var mail_switch = require('./lib/mail-switch')
@@ -16,22 +15,18 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 5000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/mail', routes.mail)
+app.get('/mail', function(req,res){ res.status(200).send('ok'); });
 app.post('/mail', mail_switch({
 	map: {
 		'detours@0circle.co.uk': 'christopherdebeer+detoursapp@gmail.com'
